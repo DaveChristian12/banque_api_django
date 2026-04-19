87af2ae84827213720440c62b484f7c791969d3a
Dave Christian <davechristian871.com>
2026-04-19 21:20:03 +0200

    Fix templates path in settings

--- banque_api/settings.py
+++ banque_api/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'banque_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'banque_api' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

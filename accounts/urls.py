0e9012bf7cafd0a8953bbd2f9490fb23704817bf
Dave Christian <davechristian871.com>
2026-04-21 22:57:19 +0200

    fix urls routing

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,22 +4,22 @@ from . import views
 urlpatterns = [
     # Health check
     path('health/', views.HealthCheckView.as_view(), name='health-check'),
-    
+
     # Database check
     path('db/', views.DatabaseCheckView.as_view(), name='db-check'),
-    
+
     # Migrations check
     path('migrations/', views.MigrationCheckView.as_view(), name='migration-check'),
-    
+
     # Liste et création de comptes
     path('', views.AccountListCreateView.as_view(), name='account-list-create'),
-    
+
     # Dépôt
     path('<uuid:account_id>/deposit/', views.DepositView.as_view(), name='deposit'),
-    
+
     # Retrait
     path('<uuid:account_id>/withdraw/', views.WithdrawView.as_view(), name='withdraw'),
-    
+
     # Détail d'un compte
     path('<uuid:account_id>/', views.AccountDetailView.as_view(), name='account-detail'),
 ]
